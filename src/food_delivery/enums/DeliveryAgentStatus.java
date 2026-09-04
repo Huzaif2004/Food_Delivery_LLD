8e759896035adad5ab000d8c3d2f48ab3313726d
Huzaif2004 <huzaifshahul112@gmail.com>
2026-09-05 00:03:17 +0530

    DeliveryAgent Entity defined

---
+++ src/food_delivery/enums/DeliveryAgentStatus.java
@@ -0,0 +1,5 @@
+package food_delivery.enums;
+
+public enum DeliveryAgentStatus {
+     ACTIVE, BUSY
+}
--- src/food_delivery/model/DeliveryAgent.java
+++ src/food_delivery/model/DeliveryAgent.java
@@ -1,5 +1,50 @@
 package food_delivery.model;
 
+import food_delivery.enums.DeliveryAgentStatus;
+
 public class DeliveryAgent {
+	private final String deliveryAgentId;
+	private String deliveryAgentName;
+	private String phoneNumber;
+	private DeliveryAgentStatus deliveryAgentStatus;
+	private double currentRating;
+	private String vehicleNumber;
+	private Location currentLocation;
+	
+	public DeliveryAgent(String deliveryAgentId, String deliveryAgentName, String phoneNumber,
+			DeliveryAgentStatus deliveryAgentStatus, double currentRating, String vehicleNumber,
+			Location currentLocation) {
+		super();
+		this.deliveryAgentId = deliveryAgentId;
+		this.deliveryAgentName = deliveryAgentName;
+		this.phoneNumber = phoneNumber;
+		this.deliveryAgentStatus = deliveryAgentStatus;
+		this.currentRating = currentRating;
+		this.vehicleNumber = vehicleNumber;
+		this.currentLocation = currentLocation;
+	}
+	public String getDeliveryAgentId() {
+		return deliveryAgentId;
+	}
+	public String getDeliveryAgentName() {
+		return deliveryAgentName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public DeliveryAgentStatus getDeliveryAgentStatus() {
+		return deliveryAgentStatus;
+	}
+	public double getCurrentRating() {
+		return currentRating;
+	}
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public Location getCurrentLocation() {
+		return currentLocation;
+	}
+	
+	
     
 }
