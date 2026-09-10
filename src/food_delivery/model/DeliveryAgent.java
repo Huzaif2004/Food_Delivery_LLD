b04067018ff9fe331e27c78361dcd69e3499466b
Huzaif2004 <huzaifshahul112@gmail.com>
2026-09-10 18:04:46 +0530

    Added methods on DeliveryAgent Service

--- src/food_delivery/model/DeliveryAgent.java
+++ src/food_delivery/model/DeliveryAgent.java
@@ -61,12 +61,41 @@ public class DeliveryAgent {
 		this.deliveryAgentStatus=DeliveryAgentStatus.BUSY;
 	}
 	public void updateRating(double rating) {
-		if(rating<0) {
+		if(rating<0||rating>5) {
 			throw new IllegalArgumentException("Rating must be between 0 and 5");
 		}
 		this.currentRating=rating;
 		
 	}
-	
+	public void updateLocation(String latitude,String longitude) {
+		if (this.currentLocation == null) {
+	        this.currentLocation = new Location(latitude, longitude); 
+		}
+		if(latitude==null||longitude==null) {
+			throw new IllegalArgumentException("Both latitude and longitude shouldn't be null");
+		}
+		currentLocation.updateLatitude(latitude);
+		currentLocation.updateLongitude(longitude);
+	}
+	public void updatePhoneNumber(String phoneNumber) {
+		if(phoneNumber==null||phoneNumber.length()!=10||!(phoneNumber.startsWith("7")||!phoneNumber.startsWith("8")||!phoneNumber.startsWith("9"))) {
+			throw new IllegalArgumentException("Invalid Phone Number");
+			
+		}
+		this.phoneNumber=phoneNumber;
+	}
+	public void updateName(String deliveryAgentName) {
+		if(deliveryAgentName==null) {
+			throw new IllegalArgumentException("Name shouldn't be null");
+		}
+		this.deliveryAgentName=deliveryAgentName;
+	}
+	public void updateVehicleNumber(String vehicleNumber) {
+		
+		if(vehicleNumber==null||!vehicleNumber.matches("^[A-Z]{2}[0-9]{2}[A-Z]{1,3}[0-9]{4}$")) {
+			throw new IllegalArgumentException("Invalid Vehicle Number");
+		}
+		this.vehicleNumber=vehicleNumber;
+	}
     
 }
